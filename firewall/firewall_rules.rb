b613bae8678728ba9b2c18e37c628a8392e79540
Yehuda Katz <wycats@gmail.com>
2012-10-31 00:18:01 -0700

    Add installation and firewall rules

---
+++ firewall/firewall_rules.rb
@@ -0,0 +1,47 @@
+require "socket"
+
+tmp = ENV["TOKAIDO_TMPDIR"]
+
+socket = "#{tmp}/tokaido-sock"
+out = "#{tmp}/tokaido-out"
+err = "#{tmp}/tokaido-err"
+
+begin
+  server = UNIXServer.open(socket)
+rescue Errno::EADDRINUSE
+  File.delete(socket)
+  retry
+end
+
+# Allow userland programs to write to this
+File.chmod(766, socket)
+
+# When this program exits, clean up the socket
+at_exit do
+  server.close
+  File.delete(socket)
+end
+
+# Redirect stdout and stderr to log files
+STDOUT.reopen(out)
+STDERR.reopen(err)
+
+puts "Tokaido Active!"
+
+while true
+  begin
+    # Listen on the socket
+    s = server.accept
+
+    # Support two commands: "enable firewall rules" and "disable firewall rules"
+    # Use rule 28561 so we can find and delete it later
+    while line = s.readline.chomp
+      if line == "enable firewall rules"
+        system "ipfw add 28561 fwd 127.0.0.1, 28561 tcp from any to me dst-port 80 in && sysctl -w net.inet.ip.forwarding=1"
+      elsif line == "disable firewall rules"
+        system "ipfw del 28561"
+      end
+    end
+  rescue EOFError
+  end
+end
