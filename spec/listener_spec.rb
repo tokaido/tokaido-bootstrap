7fedbc25cdfa02d3441a4c3896d0126acfd919fc
Andrés N. Robalino <andres@androbtech.com>
2015-04-19 13:18:29 -0500

    Listener Units.

---
+++ spec/listener_spec.rb
@@ -0,0 +1,27 @@
+require 'tokaido/bootstrap/manager'
+
+describe "Tokaido::Bootstrap::Listener" do
+  before do
+    @dirname = File.expand_path("..", __FILE__)
+
+    muxr = File.join(@dirname, "muxr.sock")
+    firewall = File.join(@dirname, "firewall.sock")
+
+    @manager = Tokaido::Bootstrap::Manager.new(muxr, firewall, @dirname)
+    @server = double(:server)
+
+    @listener = Tokaido::Bootstrap::Listener.new(@manager, @server)
+  end
+
+  it "adds a Tokaido app" do
+    expect(@manager).to receive(:add_app)
+    @listener.process_request(%{ADD "#{@dirname}" "foo.tokaido" 9292})
+  end
+
+  it "removes a Tokaido app" do
+    expect(@manager).to receive(:remove_app)
+    @listener.process_request(%{REMOVE "#{@dirname}" "foo.tokaido"})
+  end
+
+
+end
